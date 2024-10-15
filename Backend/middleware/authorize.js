385ff4559b117391cc240b55dce2e58ce1c880ce
Zaheer Mulani <154722247+RBZAHEER@users.noreply.github.com>
2024-10-15 13:30:01 +0530

    Update authorize.js

--- Backend/middleware/authorize.js
+++ Backend/middleware/authorize.js
@@ -1,41 +1,41 @@
 import jwt from "jsonwebtoken";
 import { User } from "../model/user.model.js"; // Correct the import path for User model
 
-export const authenticate = async (req, res, next) => {
-  const token = req.cookies.jwt;
-  console.log("Token received:", token);
-  if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Unauthorized. No token provided." });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
-    // console.log("Token decoded:", decoded);
-    const user = await User.findById(decoded.userId);
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized. User not found." });
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("Authentication Error:", error.message);
-    return res.status(401).json({ message: "Invalid token." });
-  }
-};
-
-// import jwt from "jsonwebtoken";
-// import { User } from "../model/user.model.js";
 // export const authenticate = async (req, res, next) => {
 //   const token = req.cookies.jwt;
+//   console.log("Token received:", token);
 //   if (!token) {
-//     return res.status(401).json({ message: "Unauthorized" });
+//     return res
+//       .status(401)
+//       .json({ message: "Unauthorized. No token provided." });
 //   }
 //   try {
 //     const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
-//     req.user = await User.findById(decoded.userId);
+//     // console.log("Token decoded:", decoded);
+//     const user = await User.findById(decoded.userId);
+//     if (!user) {
+//       return res.status(401).json({ message: "Unauthorized. User not found." });
+//     }
+//     req.user = user;
+//     next();
 //   } catch (error) {
-//     return res.status(401).json({ message: "" + error.message });
+//     console.error("Authentication Error:", error.message);
+//     return res.status(401).json({ message: "Invalid token." });
 //   }
-//   next();
 // };
+
+// import jwt from "jsonwebtoken";
+// import { User } from "../model/user.model.js";
+export const authenticate = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+    req.user = await User.findById(decoded.userId);
+  } catch (error) {
+    return res.status(401).json({ message: "" + error.message });
+  }
+  next();
+};
